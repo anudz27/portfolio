16b46173c5648d5b80d5cc38c48e35042c7778e2
Shashini Dias <117101873+anudz27@users.noreply.github.com>
2024-09-12 23:46:58 +0530

    last initial commit

--- Js/script.js
+++ Js/script.js
@@ -1,4 +1,6 @@
 const sideMenu = document.querySelector('#sideMenu');
+const navBar = document.querySelector('#navBar');
+const navLinks = document.querySelector('#navlinks');
 
 function openMenu(){
     sideMenu.style.transform = 'translatex(-16rem)';
@@ -6,3 +8,11 @@ function openMenu(){
 function closeMenu(){
     sideMenu.style.transform = 'translatex(16rem)';
 }
+window.addEventListener('scroll', ()=>{
+if(scrollY > 50){
+    navBar.classList.add('bg-white','bg-opacity-50','shadow-sm','backdrop-blur-lg');
+    navLinks.classList.remove('bg-white','shadow-sm',' bg-opacity-50');
+}else{
+    navBar.classList.remove('bg-white','bg-opacity-50','shadow-sm','backdrop-blur-lg');
+}
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 tailwind.config = {
+  export: {
     theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-        }
+      fontFamily: {
+       Outfit: ["Outfit", "sans-serif"],
+       Ovo: ['Ovo', 'serif']
+      },
+      colors:{
+        lighHover:'#F4F4F4',
+        darkHover:'#1A1A1A',
+        darkTheme:'#11001F'
       }
     }
+  },
+  darkMode:'selector'
   }
